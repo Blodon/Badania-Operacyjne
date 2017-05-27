18112dd9ed22f9e73ede71fc3c453b722f1e1d70
mikze <mikzepro@gmail.com>
2017-05-27 20:10:40 +0200

    add Simplex method

--- Optymalizacja produkcji/Optymalizacja produkcji/Form1.cs
+++ Optymalizacja produkcji/Optymalizacja produkcji/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Optymalizacja_produkcji.Methods.Linear_Methods;
 
 namespace Optymalizacja_produkcji
 {
     public partial class MainForm : Form
     {
+       
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void BUTTON_StartSimplex_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            double[] c = { 30, 20 };
+            double[,] A = { {2,1 }, {3,3}, {1.5,0} };
+            double[] b = { 1000, 2400, 600 };
+
+            Simplex simplex = new Simplex(c, A, b);
+
+            textSimplex.Text = simplex.maximize().Item1.ToString();
+        }
+
+        private void RTB_Table_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textSimplex_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
